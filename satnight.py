276070793fee5ae773ddcf953c2ede5fc2a79417
felixvalin <felix.valin@mail.mcgill.ca>
2018-02-06 11:23:33 -0500

    Created function that checks, if sun is out.

--- satnight.py
+++ satnight.py
@@ -1,2 +1,22 @@
-import datetime
-from astral import astral
+from datetime import datetime as dt
+import pytz  # To make datetime objects 'aware'
+from astral import Location
+from satgeometry import get_complete_position
+
+
+def is_night():
+    """
+    input: none
+    output: boolean night or not
+    """
+
+    utc = pytz.UTC  # UTC Object
+
+    lat, lon, city, region, time_zone = get_complete_position()
+    current_location = Location((city, region, lat, lon, time_zone, 6))
+
+    sunset = current_location.sun()['sunset']
+    sunrise = current_location.sun()['sunrise']
+    now = utc.localize(dt.now())
+
+    return sunset < now and now < sunrise
